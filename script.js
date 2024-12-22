9464e122878b49f0eb4be9bab792a95c81087791
Fatimah Azzahra Nur faidah <itzaidaa@gmail.com>
2024-12-22 17:57:14 +0700

    first card

---
+++ script.js
@@ -0,0 +1,7 @@
+function openInfo() {
+    document.getElementById('user-box').classList.remove('hide-details');
+}
+
+function closeInfo() {
+    document.getElementById('user-box').classList.add('hide-details');
+}
