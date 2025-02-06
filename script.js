db45bc6f3e65309adcfa8c78d55549088ce9ba3b
Fatimah Azzahra Nur faidah <itzaidaa@gmail.com>
2025-02-06 13:05:53 +0700

    json

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import{getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js"
-import{ renderHTML, setInner} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js"
+import { renderHTML, setInner } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
 
 getJSON("https://t.if.co.id/json/zahra.json","null","null", rensponseFunction);
 function rensponseFunction(response){
