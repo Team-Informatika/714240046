a22c608ceaa834bfccfa54bc2d5379cb88869d1e
Fatimah Azzahra Nur faidah <itzaidaa@gmail.com>
2025-02-06 12:55:36 +0700

    json

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+import{getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js"
+import{ renderHTML, setInner} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js"
+
+getJSON("https://t.if.co.id/json/zahra.json","null","null", rensponseFunction);
+function rensponseFunction(response){
+    console.log ('HTTP Status:', response.status);
+    console.log ('response.data:', response.data);
+}
 function openInfo() {
     document.getElementById('user-box').classList.remove('hide-details');
 }
