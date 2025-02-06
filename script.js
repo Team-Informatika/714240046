96094fbd10078102f2777c0d5132ff007868b0b1
Fatimah Azzahra Nur faidah <itzaidaa@gmail.com>
2025-02-06 13:27:29 +0700

    json

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 import{getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js"
-import { renderHTML, setInner } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
+import { renderHTML, setInner } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.1/element.js";
 
-fetch("https://t.if.co.id/json/zahra.json")
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error("Error fetching JSON:", error));
+getJSON("https://t.if.co.id/json/zahra.json","null","null", rensponseFunction);
+function rensponseFunction(response){
+    console.log('HTTP Status:', response.status);
+    console.log('response.data:', response.data);
+}
 
 
 
