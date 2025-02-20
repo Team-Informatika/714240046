529c1655cec625772c9223d8f9495062273fb7b7
Fatimah Azzahra Nur faidah <itzaidaa@gmail.com>
2025-02-20 16:10:48 +0700

    test

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import{getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js"
 import { renderHTML, setInner } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.1/element.js";
-getJSON("https://t.if.co.id/json/zahra.json","null","null", rensponseFunction);
-function rensponseFunction(response){
+getJSON("https://t.if.co.id/json/zahra.json",null,null, responseFunction);
+function responseFunction(response){
     console.log('HTTP Status:', response.status);
     console.log('response.data:', response.data);
     setInner('nama', response.data.card.infos.username);
